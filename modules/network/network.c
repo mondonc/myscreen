mondonc/myscreen

commit ab8593bc28733211e2f7efa15a29e19964b6ba49

remove space after 'no network found'

modules/network/network.c
@@ -239,7 +239,7 @@ char * init_network(char * confline){
 			strcpy(network_result,"selected device: ");
 		} else {
 			*interface = '\0';
-			strcpy(network_result,"no network found ");
+			strcpy(network_result,"no network found");
 		}
 
 	} else {
@@ -250,7 +250,7 @@ char * init_network(char * confline){
 	no_activity_count=0;
 	/* Build "no activity" string */
 	assert(strlen(" no activity on ")+INTERFACE_SIZE+2 <= NETWORK_RESULT_SIZE);
-	strcpy(no_activity," no activity on ");
+	strcpy(no_activity,"no activity on ");
 	strcat(no_activity,interface);
 	strcat(no_activity, " ");
 

commit e309f83ec5f5836d871dbc86c9a38d58150169b5

Fix bug introduced in ab8593bc28733211e2f7efa15a29e19964b6ba49 : the space is important to keep the same length

modules/network/network.c
@@ -250,7 +250,7 @@ char * init_network(char * confline){
 	no_activity_count=0;
 	/* Build "no activity" string */
 	assert(strlen(" no activity on ")+INTERFACE_SIZE+2 <= NETWORK_RESULT_SIZE);
-	strcpy(no_activity,"no activity on ");
+	strcpy(no_activity," no activity on ");
 	strcat(no_activity,interface);
 	strcat(no_activity, " ");
 